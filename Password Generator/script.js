7e6039187ec0dd44a18d312f76f086edabe2e918
Abhay <abhayd0331@gmail.com>
2024-12-02 21:54:55 +0530

    Ui Done

---
+++ Password Generator/script.js
@@ -0,0 +1,13 @@
+const length = document.querySelector("#length")
+const password = document.querySelector("#password")
+const displayLength = document.querySelector("#displayLength")
+const copy = document.querySelector("#copy")
+const number = document.querySelector("#number")
+const character = document.querySelector("#character")
+
+let str = `abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`
+// let allo
+
+function generatePassword(){
+
+}
